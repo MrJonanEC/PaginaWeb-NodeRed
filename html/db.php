0e736a81bc751d04412597d36ed9c19525bbd138
Jonathan Moreta <96152105+MrJonanEC@users.noreply.github.com>
2022-07-10 18:47:44 -0500

    Add files via upload

---
+++ html/db.php
@@ -0,0 +1,5 @@
+<?php
+
+$conexion=mysqli_connect("localhost","ciam","proyecto22","usuarios")or die(
+    "error de conexion");
+?>
---
+++ html/home.php
@@ -0,0 +1,18 @@
+
+
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>home</title>
+</head>
+<body>
+<h1>SISTEMA DOMOTICO</h1>
+<h2>Universidad tecnica de Ambato</h2>
+<h3>Facultad de Ingeniería en Sistemas, Electrónica e Industrial</h3>
+<h3>Carrera de Ingeniería en Electrónica y Comunicaciones</h3>
+    
+</body>
+</html>
